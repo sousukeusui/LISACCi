0e3a3f4f4621c9dbb5a32e13671597da721c0d62
sousuke <suguruyoshida.0807@gmail.com>
2022-10-31 23:07:32 +0900

    仮ユーザ登録の保存機能作成
    仮ユーザー登録で保存機能の作成（一応データベースに保存できる状態）

--- app/controllers/tentative_users_controller.rb
+++ app/controllers/tentative_users_controller.rb
@@ -4,6 +4,25 @@ class TentativeUsersController < ApplicationController
     end
 
     def create
-        p '成功'
+        @tentative_user = TentativeUser.find_or_initialize_by(tentative_user)
+        @tentative_user.token = SecureRandom.base64
+        @tentative_user.expired_at = DateTime.now
+
+        if @tentative_user.save
+            redirect_to tentative_users_after_new_path, notice: 'メールが送信されました'
+        else
+            render :new
+        end
+    end
+
+    def after_new
+
+    end
+
+
+    private
+    
+    def tentative_user
+        params.require(:tentative_user).permit(:mail)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'tentative_users/new'
   post 'tentative_users/create'
+  get 'tentative_users/after_new'
   get 'users/new'
   get '/', to: 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
