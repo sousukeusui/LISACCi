370bd6b80986756da16fa12f9129832a44cb7741
sousuke <suguruyoshida.0807@gmail.com>
2022-10-31 22:28:25 +0900

    ルーティングの変更
    仮登録を行う際のフォームのpost送信のルーティング変更

--- app/controllers/tentative_users_controller.rb
+++ app/controllers/tentative_users_controller.rb
@@ -2,4 +2,8 @@ class TentativeUsersController < ApplicationController
     def new
         @tentative_user = TentativeUser.new
     end
+
+    def create
+        p '成功'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'tentative_users/new'
-  post 'tentative_users/new'
+  post 'tentative_users/create'
   get 'users/new'
   get '/', to: 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
