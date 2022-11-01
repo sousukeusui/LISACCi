36e26cd233e299ac6018dbc7614cb35f025f7e1e
sousuke <suguruyoshida.0807@gmail.com>
2022-11-01 09:02:12 +0900

    仮ユーザー登録機能のトークン生成機能作成
    仮ユーザー登録する際、メール認証のためのトークンを生成する機能の作成

--- app/controllers/tentative_users_controller.rb
+++ app/controllers/tentative_users_controller.rb
@@ -5,7 +5,7 @@ class TentativeUsersController < ApplicationController
 
     def create
         @tentative_user = TentativeUser.find_or_initialize_by(tentative_user)
-        @tentative_user.token = SecureRandom.base64
+        @tentative_user.token = create_token
         @tentative_user.expired_at = DateTime.now
 
         if @tentative_user.save
@@ -21,6 +21,17 @@ class TentativeUsersController < ApplicationController
 
 
     private
+
+    def create_token
+        token = nil
+        loop do
+            token = SecureRandom.base64
+            if TentativeUser.find_by(token: token).nil?
+                break
+            end
+        end
+        return token
+    end
     
     def tentative_user
         params.require(:tentative_user).permit(:mail)
