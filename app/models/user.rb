49440e5474a06ba256e3923eedc71aef5ae3d242
sousuke <suguruyoshida.0807@gmail.com>
2022-11-01 22:49:39 +0900

    [add]ユーザーモデルにバリデーション追加
    user.rbにバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: {maximum: 16}
+    validates :mail, presence: true, uniqueness: true
+    validates :password_digest, presence: true, uniqueness: true
+    validates :account_id, presence: true, length: {maximum: 16}
+    #validates :image, presence: ture
+    validates :self_introduction, length: {maximum: 100}
+    validates :position, length: {maximum: 10}
+    validates :mail, format: {with: /^[\w\-.]+@[a-z\d\-.]+.[a-z]+(.[a-z]+)?$/}
 end
