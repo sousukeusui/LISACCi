4a76ab87217f8c8fa4883c382a8cc9f60bee3c44
sousuke <suguruyoshida.0807@gmail.com>
2022-11-05 21:40:42 +0900

    [fix]仮ユーザーモデルのバリデーション修正
    仮ユーザーモデルのバリデーションでエラーが発生したため修正

--- app/models/tentative_user.rb
+++ app/models/tentative_user.rb
@@ -1,4 +1,4 @@
 class TentativeUser < ApplicationRecord
-    validates :mail, presence: ture, uniqueness: true
-    validates :mail, format: {with: /^[\w\-.]+@[a-z\d\-.]+.[a-z]+(.[a-z]+)?$/}
+    validates :mail, presence: true, uniqueness: true
+    validates :mail, format: {with: /\A[\w\-.]+@[a-z\d\-.]+.[a-z]+(.[a-z]+)?\z/}
 end
