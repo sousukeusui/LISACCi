026d567423177b78e428ea4a2a581dd0d46f135c
sousuke <suguruyoshida.0807@gmail.com>
2022-11-05 17:16:32 +0900

    [update]仮ユーザー登録時のトークン生成機能の修正
    仮ユーザー登録時にURL-safeなトークンを作れるように修正

--- app/controllers/tentative_users_controller.rb
+++ app/controllers/tentative_users_controller.rb
@@ -4,11 +4,17 @@ class TentativeUsersController < ApplicationController
     end
 
     def create
+        #メールアドレスが登録されていなければ登録
         @tentative_user = TentativeUser.find_or_initialize_by(tentative_user)
+        #トークン生成
         @tentative_user.token = create_token
+        #現在時刻の登録
         @tentative_user.expired_at = DateTime.now
 
         if @tentative_user.save
+            #メールを送る処理を書く
+
+
             redirect_to tentative_users_after_new_path, notice: 'メールが送信されました'
         else
             render :new
@@ -22,10 +28,10 @@ class TentativeUsersController < ApplicationController
 
     private
 
-    def create_token
+    def create_token #トークンを作るメソッド
         token = nil
-        loop do
-            token = SecureRandom.base64
+        loop do #同じトークンがある場合作り直す
+            token = SecureRandom.urlsafe_base64
             if TentativeUser.find_by(token: token).nil?
                 break
             end
