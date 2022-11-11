f553db3ad6af400785eb590102cfe51a8c93be60
sousuke <suguruyoshida.0807@gmail.com>
2022-11-11 22:37:28 +0900

    [add]仮ユーザー登録時のトークン正誤判断機能追加
    本登録時に仮ユーザを登録したときのトークンが正しいか、誤りかで条件分岐し表示するviewも分岐

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def new
     #仮登録時のトークンが正しいかどうか、有効期限が切れてないか調べる
-    token = params[:token]
-    p token
+    if TentativeUser.find_by(token: params[:token])
+      @user = User.new
+    else
+      render action: :token_error
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
     #validates :image, presence: ture
     validates :self_introduction, length: {maximum: 100}
     validates :position, length: {maximum: 10}
-    validates :mail, format: {with: /^[\w\-.]+@[a-z\d\-.]+.[a-z]+(.[a-z]+)?$/}
+    validates :mail, format: {with: /\A[\w\-.]+@[a-z\d\-.]+.[a-z]+(.[a-z]+)?\z/}
 end
