ea8c66093c11abf335bd1f9e125b0d496d3f7379
sousuke <suguruyoshida.0807@gmail.com>
2022-11-12 22:57:42 +0900

    [add]仮ユーザーの期限機能追加
    本登録時のURLを押した際に、仮ユーザー登録から一定期間過ぎていたら無効になる機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def new
     #仮登録時のトークンが正しいかどうか、有効期限が切れてないか調べる
-    if TentativeUser.find_by(token: params[:token])
+    tentative_user = TentativeUser.find_by(token: params[:token])
+    if tentative_user != nil && tentative_user.expired_at > Time.zone.now
       @user = User.new
     else
       render action: :token_error
