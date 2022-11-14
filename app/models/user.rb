5e6fffa45e33af516a73ae4ff848e8391d513454
sousuke <suguruyoshida.0807@gmail.com>
2022-11-14 22:48:30 +0900

    [add]ユーザモデルにhas_secure_password追加
    ユーザーモデルにhas_secure_passwordを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     validates :name, presence: true, length: {maximum: 16}
     validates :mail, presence: true, uniqueness: true
     validates :password_digest, presence: true, uniqueness: true
