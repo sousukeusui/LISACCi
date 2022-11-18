1807d6c9f94a4789264d7fbd6f6759fb6fff3fb8
sousuke <suguruyoshida.0807@gmail.com>
2022-11-18 23:05:22 +0900

    [add]本登録のアカウントid生成機能追加
    本登録時にフォームにアカウントidを入力したら入力された値を使用し、何も入力しなかったらアカウントidを自動生成する機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,21 +10,29 @@ class UsersController < ApplicationController
   end
 
   def create
+    #account_idのパラメーターがあるなら入力されたacount_id、ないなら自動生成
+    if params[:user][:account_id].present?
       @user = User.new(user_params)
-      #account_idのパラメーターがnullならacount_idを自動生成
+    else
+      @user = User.new(user_params_account_id_nil)
       @user.account_id = SecureRandom.alphanumeric(8)
-      #デフォルトのイメージ保存
-      @user.image = "image"
+    end
+    #デフォルトのイメージ保存
+    @user.image = "image"
 
-      #userを保存に成功時、現場一覧に遷移
-      if @user.save
-        redirect_to tentative_users_after_new_path
-      end
+    #userを保存に成功時、現場一覧に遷移
+    if @user.save
+      redirect_to tentative_users_after_new_path
+    end
   end
 
 
   private
   def user_params
-      params.require(:user).permit(:name, :mail, :password, :password_confirmation)
+    params.require(:user).permit(:name, :mail, :password, :password_confirmation, :account_id)
+  end
+
+  def user_params_account_id_nil
+    params.require(:user).permit(:name, :mail, :password, :password_confirmation)
   end
 end
