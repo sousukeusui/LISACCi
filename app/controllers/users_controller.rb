d17e33dd7012765d77fff2451a2816ba7258ba88
sousuke <suguruyoshida.0807@gmail.com>
2022-11-21 22:38:36 +0900

    [clean]users_controller内のリファクタリング
    users_controllerのnewメソッド内の分岐の処理を読みやすいように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new
     #仮登録時のトークンが正しいかどうか、有効期限が切れてないか調べる
     @tentative_user = TentativeUser.find_by(token: params[:token])
-    if @tentative_user != nil && @tentative_user.expired_at > Time.zone.now
+    if @tentative_user.present? && @tentative_user.expired_at > Time.zone.now
       @user = User.new
     else
       render action: :token_error
