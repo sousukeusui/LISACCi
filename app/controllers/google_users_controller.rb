eee5384ff82389d0f7bc1c3fc85eb3b4864e6fc6
sousuke <suguruyoshida.0807@gmail.com>
2022-11-28 22:16:02 +0900

    [update]アクション名変更
    google_usersコントローラー内のregistrationアクションをcreateアクションに変更。それに伴うルーティングも変更

--- app/controllers/google_users_controller.rb
+++ app/controllers/google_users_controller.rb
@@ -1,10 +1,10 @@
 class GoogleUsersController < ApplicationController
     require 'googleauth/id_tokens/verifier'
 
-    protect_from_forgery except: :registration
+    protect_from_forgery except: :create
     before_action :verify_g_csrf_token
 
-    def registration
+    def create
         payload = Google::Auth::IDTokens.verify_oidc(params[:credential], aud: ENV['GOOGLE_CLIENT_ID'])
         google_user = GoogleUser.find_or_create_by(mail: payload['email'])
         session[:user_id] = google_user.id
