d852cf55acdb5f7ccd2ded54df43e4e2758fbca6
sousuke <suguruyoshida.0807@gmail.com>
2022-12-01 22:34:26 +0900

    [add]ログインフォームの作成
    ログインフォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get '/', to: 'top#index'
   post 'google_login_api/callback', to: 'google_users#login'
   get 'sessions/new'
+  post 'sessions/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
