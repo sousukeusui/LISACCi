851c538e6a27816605495fe03491e87af9a989bf
sousuke <suguruyoshida.0807@gmail.com>
2022-12-03 22:16:51 +0900

    [add]ログイン機能の作成
    ログイン機能の作成。ログイン成功時にセッションを渡す処理をする。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
     def new
-			
+			@user = User.new
     end
+
+		def create
+			@user = User.find_by(mail: params[:mail])
+			if @user && @user.authenticate(params[:password])
+				session[:user_id] = @user.id
+				redirect_to tentative_users_after_new_path ,notice: 'ログインしました'#工事一覧画面に遷移させるため後ほどpath変える
+			else
+				render action: :new
+			end
+		end
+
+		private 
+		def sessions_params
+			params.require(:user).permit(:mail, :password, :password_confirmation)
+		end
 end
