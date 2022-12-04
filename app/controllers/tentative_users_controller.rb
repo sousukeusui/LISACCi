a86c9522aa561a1a97c9a10a644045eecdb809e6
sousuke <suguruyoshida.0807@gmail.com>
2022-12-04 16:44:34 +0900

    [add]ログイン機能で使用するモジュールの追加
    ログインの有無など容易に判定できるモジュールの追加。

--- app/controllers/tentative_users_controller.rb
+++ app/controllers/tentative_users_controller.rb
@@ -1,21 +1,21 @@
 class TentativeUsersController < ApplicationController
     def new
-        @tentative_user = TentativeUser.new
+			@tentative_user = TentativeUser.new
     end
 
     def create
-        #仮ユーザー作る
-        @tentative_user = TentativeUser.new(tentative_user)
-        #期限時間の登録
-        @tentative_user.expired_at = Time.zone.now + 1.days
+			#仮ユーザー作る
+			@tentative_user = TentativeUser.new(tentative_user)
+			#期限時間の登録
+			@tentative_user.expired_at = Time.zone.now + 1.days
 
-        if @tentative_user.save
-            #メールを送る処理を書く
-            UserMailer.registration_mail(@tentative_user).deliver_later
-            redirect_to tentative_users_after_new_path, notice: 'メールが送信されました'
-        else
-            render :new
-        end
+			if @tentative_user.save
+					#メールを送る処理を書く
+				UserMailer.registration_mail(@tentative_user).deliver_later
+				redirect_to tentative_users_after_new_path, notice: 'メールが送信されました'
+			else
+				render :new
+			end
     end
 
     def after_new
@@ -26,6 +26,6 @@ class TentativeUsersController < ApplicationController
     private
     
     def tentative_user
-        params.require(:tentative_user).permit(:mail)
+			params.require(:tentative_user).permit(:mail)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+	#ログインしているユーザーを取得
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		end
+	end
+
+	#渡されたuserとログインしているユーザーが一致すればtrueを返す
+	def current_user?(user)
+		user == current_user
+	end
+
+	#ログインしていればtureを返す
+	def logged_in
+		current_user.present?
+	end
 end
