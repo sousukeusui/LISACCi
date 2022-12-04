e69a95bb861ff5fdbf793942a6aac5a14bca4abe
sousuke <suguruyoshida.0807@gmail.com>
2022-12-04 17:07:43 +0900

    [clean]userモデル内のメソッドのリファクタリング
    userモデル内のメソッドのリファクタリングと、本登録時にセッションを渡す処理追加。

--- app/models/tentative_user.rb
+++ app/models/tentative_user.rb
@@ -1,15 +1,11 @@
 class TentativeUser < ApplicationRecord
     before_create do
-        #新規保存前にトークン生成
-        self.token = SecureRandom.urlsafe_base64
+			#新規保存前にトークン生成
+			self.token = SecureRandom.urlsafe_base64
     end
 
     def has_registration_data
-        if self.present? && self.expired_at > Time.zone.now
-            return true
-        else
-            return false
-        end
+      self.present? && self.expired_at > Time.zone.now
     end
     validates :mail, presence: true, uniqueness: true
     validates :mail, format: {with: /\A[\w\-.]+@[a-z\d\-.]+.[a-z]+(.[a-z]+)?\z/}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+
     validates :name, presence: true, length: {maximum: 16}
     validates :mail, presence: true, uniqueness: true
     validates :password_digest, presence: true, uniqueness: true
