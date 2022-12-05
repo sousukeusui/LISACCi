6cce392405058b312e984105b302c51618255ef9
sousuke <suguruyoshida.0807@gmail.com>
2022-12-05 22:37:19 +0900

    [add]siteモデルにバリデーションの追加
    バリデーションの追加

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,9 @@
 class Site < ApplicationRecord
+  validates :customer, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :content, presence: true
+  validates :construction_date, presence: true
+  validates :progress, presence: true
+  validates :check, presence: true
 end
