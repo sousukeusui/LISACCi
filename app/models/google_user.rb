358a9d8039b021fe33647bc95e97a3e57610ad8d
sousuke <suguruyoshida.0807@gmail.com>
2022-12-18 17:17:16 +0900

    [fix]インデント再修正

--- app/models/google_user.rb
+++ app/models/google_user.rb
@@ -1,6 +1,6 @@
 class GoogleUser < ApplicationRecord
-	#has_many :user_sites
-	#has_many :sites, through: :user_sites
+  #has_many :user_sites
+  #has_many :sites, through: :user_sites
 
-	validates :mail, presence: true, uniqueness: true
+  validates :mail, presence: true, uniqueness: true
 end
